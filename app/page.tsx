fa41318903066b6d1d4dbc29df2832519607226c
luisnavedag <luisjosenavedagarcia@gmail.com>
2023-09-21 20:04:49 -0300

    Creating my first pages

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1>Soy la página principal</h1>
+  )
+}
---
+++ components/Navigation.tsx
@@ -0,0 +1,33 @@
+import styles from "./Navigation.module.css"
+import Link from "next/link"
+const links = [
+    {
+        label: "Home",
+        route: "/"
+    },
+    {
+        label: "About",
+        route: "/about"
+    }
+]
+
+
+export default function Navigation(){
+    return(
+        <header className={styles.header}>
+            <nav>
+                <ul>
+                    {
+                        links.map(({label, route}) => (
+                            <li key={route}>
+                                <Link href={route}>
+                                    {label}
+                                </Link>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </nav>
+        </header>
+    )
+}
